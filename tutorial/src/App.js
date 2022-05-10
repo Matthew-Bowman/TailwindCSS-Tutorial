0facae4ae4a14b328e108b45dd87da084ef36cfd
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-05-10 22:58:23 +0100

    Tutorial Completed

---
+++ tutorial/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import SideBar from "./SideBar";
+
+function App() {
+  return (
+    <div className="flex">
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
