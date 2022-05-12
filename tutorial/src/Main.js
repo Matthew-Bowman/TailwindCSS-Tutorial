fc0d38284db46744e73cf892306cac6667d1df65
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-05-12 18:52:57 +0100

    Created Main

---
+++ tutorial/src/Main.js
@@ -0,0 +1,11 @@
+const Main = () => {
+    return (
+        <div className="w-full h-screen
+                        fixed left-80
+                        bg-gray-700">
+
+        </div>
+    );
+}
+
+export default Main;
