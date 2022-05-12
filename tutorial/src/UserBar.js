6e4cf85f9938b261fbb723bf6e87180c8b244c99
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-05-12 18:53:06 +0100

    Created User Bar

---
+++ tutorial/src/UserBar.js
@@ -0,0 +1,11 @@
+const UserBar = () => {
+    return (
+        <div className="fixed right-0 
+                        w-64 h-screen
+                        bg-gray-800">
+
+        </div>
+    );
+}
+
+export default UserBar;
