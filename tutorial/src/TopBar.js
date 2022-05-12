f2db6ca5c630c5213309c1821105348052c383c4
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-05-12 21:55:24 +0100

    Created Top Bar

---
+++ tutorial/src/TopBar.js
@@ -0,0 +1,13 @@
+const TopBar = ({bg, ug, heading}) => {
+    return (
+        <div className={`w-full h-auto
+                        bg-gray-${bg} 
+                        shadow-md shadow-gray-${ug}`}>
+            <h2 className="ml-4 mb-2 
+                           py-2
+                           text-xl text-gray-100">{heading}</h2>
+        </div>
+    );
+}
+
+export default TopBar;
