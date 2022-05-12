5402e6ec6e124a40341c0ac79ac1887f61f53645
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-05-12 21:55:58 +0100

    Added Top Bar

--- tutorial/src/Main.js
+++ tutorial/src/Main.js
@@ -1,9 +1,11 @@
+import TopBar from "./TopBar"
+
 const Main = () => {
     return (
         <div className="w-full h-screen
                         fixed left-80
                         bg-gray-700">
-
+            <TopBar bg="700" ug="800" heading="General"/>
         </div>
     );
 }
