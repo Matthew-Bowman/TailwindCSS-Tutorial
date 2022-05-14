02b4bd9c19807f7c776081da895775425cad3738
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-05-14 23:56:46 +0100

    Created Message Component Items

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,8 @@ const Main = () => {
         <div className="w-full h-screen
                         fixed left-80
                         bg-gray-700">
-            <TopBar bg="700" ug="800" heading="General"/>
+            <TopBar bg="700" ug="800" heading="General" />
+            <Message />
         </div>
     );
 }
@@ -13,7 +14,9 @@ const Main = () => {
 const Message = () => {
     return (
         <div>
-            <h1>Test</h1>
+            <img src="https://picsum.photos/200" width="64" height="64" />
+            <h3>Lorem Ipsum</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     );
 };
