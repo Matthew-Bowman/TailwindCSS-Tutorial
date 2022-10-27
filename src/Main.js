eb1e6c708543738abd71867118ea017a13bba723
Matthew.Bowman <bowm2_21@uni.worc.ac.uk>
2022-10-27 20:02:29 +0100

    Changed text in messages & component styling

--- src/Main.js
+++ src/Main.js
@@ -8,11 +8,14 @@ const Main = () => {
                         right-0
                         flex-grow flex-shrink-0
                         bg-gray-700">
+            {/* Heading */}
             <TopBar bg="700" ug="800" heading="General" />
-            <div className="overflow-y-scroll scrollbar-none h-full mb-16">
-                <Message avatar="https://picsum.photos/48?random=1" author="Matthew." text="Hello! This is some really long text which will allow me to see how well the text wraps onto a new line once the message gets too long. :)" />
-                <Message avatar="https://picsum.photos/48?random=2" author="Kheraa" text="Oh yeah, no worries. I understand what you're doing! :)" />
+            {/* Message Container */}
+            <div className="overflow-y-scroll scrollbar-none h-full">
+                <Message avatar="https://picsum.photos/48?random=1" author="Matthew." text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultricies mi quis hendrerit dolor magna eget est. Aenean pharetra magna ac placerat vestibulum lectus mauris. Amet nisl suscipit adipiscing bibendum." />
+                <Message avatar="https://picsum.photos/48?random=2" author="Kheraa" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum sed arcu non odio euismod lacinia at quis. Amet nisl suscipit adipiscing bibendum est. Mauris vitae ultricies leo integer malesuada nunc vel." />
             </div>
+            {/* Where Users Type Their Message */}
             <MessageBar />
         </div>
     );
@@ -20,8 +23,15 @@ const Main = () => {
 
 const MessageBar = () => {
     return (
-        <div className="h-16 fixed bottom-0 w-full bg-gray-800">
-        </div>
+        <form className="h-auto
+                        flex align-middle justify-center
+                        bg-gray-700" id="message-form">
+            <input  className="h-12 w-full
+                               px-2 m-3 
+                               rounded-lg 
+                               focus:outline-none 
+                               bg-gray-600 text-gray-100" id="message-content" type="text" placeholder="Enter your message..."/>
+        </form>
     );
 }
 
@@ -31,7 +41,7 @@ const Message = ({avatar, author, text}) => {
             <img className="rounded-full h-12 w-12 mr-4" src={`${avatar}`} width="48" height="48" />
             <div className="">
                 <h3 className="font-semibold text-base text-gray-200">{author}</h3>
-                <p className="text-sm text-gray-400">{text}</p>
+                <p className="text-sm text-gray-400 break-all">{text}</p>
             </div>
         </div>
     );
